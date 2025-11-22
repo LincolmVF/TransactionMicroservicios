a4806f53c5365cb9b4690958be48f07e31122b79
Lincolm Villanueva <lincolmvf@gmail.com>
2025-11-21 21:18:05 -0500

    Contraseña de RADIS

--- transaction-service-express/config/redis.js
+++ transaction-service-express/config/redis.js
@@ -6,6 +6,7 @@ const Redis = require('ioredis');
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
 });
 
 // Exportamos el cliente
